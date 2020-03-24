fe63b8d886fa8ed7b27e2e26f0240f17f6d0d2c9
Akhil Seshan <akhilses@gmail.com>
2020-03-24 13:15:42 +0530

    login

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Login = () => {
-    return (
-        <div className="container-fluid">
-            <h1>Login section</h1>
-        </div>
-    )
+class Login extends Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                {/* <div className="row">
+                    <main role="main" className="col-lg-12 d-flex justify-content-center">
+                        <div id="loader" className="text-center">
+                            <p className="text-center">Loading...</p>
+                        </div>
+                        <div id="content">
+                            <p> */}
+                                <h1>
+                                    <center>
+                                        Certify
+                                    </center>
+                                </h1>
+                            {/* </p>
+                            <form onSubmit="######">
+                                <select id="user_profile" className="profile">
+                                    <option value="Student">Student</option>
+                                    <option value="University">University</option>
+                                    <option value="Organisation">Company</option>
+                                </select>
+                                <p>
+                                </p>
+                                <input id="user_uid" type="text" className="form-control" placeholder="Your UID" required></input>
+                                <input id="user_name" type="text" className="form-control" placeholder="Your Name" required></input>
+                                <input id="user_email" type="email" className="form-control" placeholder="Your Email" required></input>
+                                <center>
+                                    <button id="but" type="submit">
+                                        Register
+                                </button>
+                                </center>
+                            </form>
+                            <p>Already Registered? 
+                                <center>
+                                    <button id="but" type="submit" onClick="####">
+                                        Login
+                                    </button>
+                                </center>
+                            </p>
+                            <ul id="certList" className="list-unstyled">
+                                <div className="template" className="checkbox" style="display: none;">
+                                    <label>
+                                        <span className="content">
+                                            content goes here....
+                                        </span>
+                                    </label>
+                                </div>
+                            </ul>
+                            <ul id="completedTaskList" className="list-unstyled">
+                            </ul>
+                        </div>
+                    </main>
+                </div> */}
+            </div>
+        )
+    }
 }
 
 export default Login;
